8bcf358c6b7b027e65f09c15f34c65266aea042b
Benzema9Real <footballfceasports>
2024-03-30 16:12:36 +0600

    dghhf

--- main/forms.py
+++ main/forms.py
@@ -1,5 +1,14 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm
+from .models import Inventar
+
+
+class BookForm(ModelForm):
+    class Meta:
+        model = Inventar
+        fields = '__all__'
+        exclude = ['slug']
 
 
 class UserForm(UserCreationForm):
--- main/views.py
+++ main/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Employee, Inventar
 from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.views.generic import DetailView
 from django.views.generic.edit import CreateView
 
 from .forms import UserForm
@@ -14,10 +14,15 @@ def index(request):
     return render(request, "index.html")
 
 
-
-
 class UserRegister(CreateView):
     model = User
     template_name = 'registration/reg.html'
     form_class = UserForm
     success_url = reverse_lazy('login')
+
+
+class MyDetailView(DetailView):
+    model = Inventar
+    template_name = 'detail/detail.html'
+    context_object_name = 'inventar'
+    slug_field = 'slug'
