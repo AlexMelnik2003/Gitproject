0c05e7f08224558d3971ea78e54efd3da40ddc24
Benzema9Real <footballfceasports>
2024-05-02 20:14:23 +0600

    bnnmvcfgfgdfg

--- main/forms.py
+++ main/forms.py
@@ -27,5 +27,5 @@ class RegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2', 'captcha')
+        fields = ('username', 'email', 'password1', 'password2')
 
