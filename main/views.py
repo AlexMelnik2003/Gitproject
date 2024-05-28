59d3c2b0a04e1840724ff0b93aa04ce45a72e973
D1M1TR1Y <dimaharcenko513@gmail.com>
2024-05-28 22:51:32 +0600

    hguiguio

--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
