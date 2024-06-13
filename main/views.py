3849af113f23bf79390af773d404d1ddcf63253f
Benzema9Real <footballfceasports>
2024-06-13 20:09:54 +0600

    wert

--- main/views.py
+++ main/views.py
@@ -109,7 +109,7 @@ def category_forms(request):
 @login_required
 def employee(request):
     employees = Employee.objects.all().filter(user=request.user)
-    paginator = Paginator(employees, 6)
+    paginator = Paginator(employees, 8)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     return render(request, 'employee.html', {'page_obj': page_obj})
